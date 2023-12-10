a3fa151aea1632c1261c62f99a490551340a66a7
Andreas Motl <andreas.motl@crate.io>
2023-12-10 01:40:15 +0100

    Add software tests, unmodified, from MeltanoLabs' `tap-postgres`
    https://github.com/MeltanoLabs/tap-postgres

---
+++ tests/test_core.py
@@ -0,0 +1,409 @@
+import copy
+import datetime
+import decimal
+import json
+
+import pendulum
+import pytest
+import sqlalchemy
+from faker import Faker
+from singer_sdk.testing import get_tap_test_class, suites
+from singer_sdk.testing.runners import TapTestRunner
+from sqlalchemy import Column, DateTime, Integer, MetaData, Numeric, String, Table, text
+from sqlalchemy.dialects.postgresql import BIGINT, DATE, JSON, JSONB, TIME, TIMESTAMP
+from test_replication_key import TABLE_NAME, TapTestReplicationKey
+from test_selected_columns_only import (
+    TABLE_NAME_SELECTED_COLUMNS_ONLY,
+    TapTestSelectedColumnsOnly,
+)
+
+from tap_postgres.tap import TapPostgres
+
+SAMPLE_CONFIG = {
+    "start_date": pendulum.datetime(2022, 11, 1).to_iso8601_string(),
+    "sqlalchemy_url": "postgresql://postgres:postgres@localhost:5432/postgres",
+}
+
+NO_SQLALCHEMY_CONFIG = {
+    "start_date": pendulum.datetime(2022, 11, 1).to_iso8601_string(),
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",
+    "password": "postgres",
+    "database": "postgres",
+}
+
+
+def setup_test_table(table_name, sqlalchemy_url):
+    """setup any state specific to the execution of the given module."""
+    engine = sqlalchemy.create_engine(sqlalchemy_url, future=True)
+    fake = Faker()
+
+    date1 = datetime.date(2022, 11, 1)
+    date2 = datetime.date(2022, 11, 30)
+    metadata_obj = MetaData()
+    test_replication_key_table = Table(
+        table_name,
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("updated_at", DateTime(), nullable=False),
+        Column("name", String()),
+    )
+    with engine.begin() as conn:
+        metadata_obj.create_all(conn)
+        conn.execute(text(f"TRUNCATE TABLE {table_name}"))
+        for _ in range(1000):
+            insert = test_replication_key_table.insert().values(
+                updated_at=fake.date_between(date1, date2), name=fake.name()
+            )
+            conn.execute(insert)
+
+
+def teardown_test_table(table_name, sqlalchemy_url):
+    engine = sqlalchemy.create_engine(sqlalchemy_url, future=True)
+    with engine.begin() as conn:
+        conn.execute(text(f"DROP TABLE {table_name}"))
+
+
+custom_test_replication_key = suites.TestSuite(
+    kind="tap", tests=[TapTestReplicationKey]
+)
+
+custom_test_selected_columns_only = suites.TestSuite(
+    kind="tap", tests=[TapTestSelectedColumnsOnly]
+)
+
+TapPostgresTest = get_tap_test_class(
+    tap_class=TapPostgres,
+    config=SAMPLE_CONFIG,
+    catalog="tests/resources/data.json",
+    custom_suites=[custom_test_replication_key],
+)
+
+TapPostgresTestNOSQLALCHEMY = get_tap_test_class(
+    tap_class=TapPostgres,
+    config=NO_SQLALCHEMY_CONFIG,
+    catalog="tests/resources/data.json",
+    custom_suites=[custom_test_replication_key],
+)
+
+
+# creating testing instance for isolated table in postgres
+TapPostgresTestSelectedColumnsOnly = get_tap_test_class(
+    tap_class=TapPostgres,
+    config=SAMPLE_CONFIG,
+    catalog="tests/resources/data_selected_columns_only.json",
+    custom_suites=[custom_test_selected_columns_only],
+)
+
+
+class TestTapPostgres(TapPostgresTest):
+    table_name = TABLE_NAME
+    sqlalchemy_url = SAMPLE_CONFIG["sqlalchemy_url"]
+
+    @pytest.fixture(scope="class")
+    def resource(self):
+        setup_test_table(self.table_name, self.sqlalchemy_url)
+        yield
+        teardown_test_table(self.table_name, self.sqlalchemy_url)
+
+
+class TestTapPostgres_NOSQLALCHMY(TapPostgresTestNOSQLALCHEMY):
+    table_name = TABLE_NAME
+    sqlalchemy_url = SAMPLE_CONFIG["sqlalchemy_url"]
+
+    @pytest.fixture(scope="class")
+    def resource(self):
+        setup_test_table(self.table_name, self.sqlalchemy_url)
+        yield
+        teardown_test_table(self.table_name, self.sqlalchemy_url)
+
+
+class TestTapPostgresSelectedColumnsOnly(TapPostgresTestSelectedColumnsOnly):
+    table_name = TABLE_NAME_SELECTED_COLUMNS_ONLY
+    sqlalchemy_url = SAMPLE_CONFIG["sqlalchemy_url"]
+
+    @pytest.fixture(scope="class")
+    def resource(self):
+        setup_test_table(self.table_name, self.sqlalchemy_url)
+        yield
+        teardown_test_table(self.table_name, self.sqlalchemy_url)
+
+
+def test_temporal_datatypes():
+    """Dates were being incorrectly parsed as date times (issue #171).
+
+    This test checks that dates are being parsed correctly, and additionally implements
+    schema checks, and performs similar tests on times and timestamps.
+    """
+    table_name = "test_temporal_datatypes"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("column_date", DATE),
+        Column("column_time", TIME),
+        Column("column_timestamp", TIMESTAMP),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(
+            column_date="2022-03-19",
+            column_time="06:04:19.222",
+            column_timestamp="1918-02-03 13:00:01",
+        )
+        conn.execute(insert)
+    tap = TapPostgres(config=SAMPLE_CONFIG)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "FULL_TABLE"
+
+    test_runner = PostgresTestRunner(
+        tap_class=TapPostgres, config=SAMPLE_CONFIG, catalog=tap_catalog
+    )
+    test_runner.sync_all()
+    for schema_message in test_runner.schema_messages:
+        if (
+            "stream" in schema_message
+            and schema_message["stream"] == altered_table_name
+        ):
+            assert (
+                "date"
+                == schema_message["schema"]["properties"]["column_date"]["format"]
+            )
+            assert (
+                "date-time"
+                == schema_message["schema"]["properties"]["column_timestamp"]["format"]
+            )
+    assert test_runner.records[altered_table_name][0] == {
+        "column_date": "2022-03-19",
+        "column_time": "06:04:19.222000",
+        "column_timestamp": "1918-02-03T13:00:01",
+    }
+
+
+def test_jsonb_json():
+    """JSONB and JSON Objects weren't being selected, make sure they are now"""
+    table_name = "test_jsonb_json"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("column_jsonb", JSONB),
+        Column("column_json", JSON),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(
+            column_jsonb={"foo": "bar"},
+            column_json={"baz": "foo"},
+        )
+        conn.execute(insert)
+    tap = TapPostgres(config=SAMPLE_CONFIG)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "FULL_TABLE"
+
+    test_runner = PostgresTestRunner(
+        tap_class=TapPostgres, config=SAMPLE_CONFIG, catalog=tap_catalog
+    )
+    test_runner.sync_all()
+    for schema_message in test_runner.schema_messages:
+        if (
+            "stream" in schema_message
+            and schema_message["stream"] == altered_table_name
+        ):
+            assert schema_message["schema"]["properties"]["column_jsonb"] == {}
+            assert schema_message["schema"]["properties"]["column_json"] == {}
+    assert test_runner.records[altered_table_name][0] == {
+        "column_jsonb": {"foo": "bar"},
+        "column_json": {"baz": "foo"},
+    }
+
+
+def test_decimal():
+    """Schema was wrong for Decimal objects. Check they are correctly selected."""
+    table_name = "test_decimal"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("column", Numeric()),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(column=decimal.Decimal("3.14"))
+        conn.execute(insert)
+        insert = table.insert().values(column=decimal.Decimal("12"))
+        conn.execute(insert)
+        insert = table.insert().values(column=decimal.Decimal("10000.00001"))
+        conn.execute(insert)
+    tap = TapPostgres(config=SAMPLE_CONFIG)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "FULL_TABLE"
+
+    test_runner = PostgresTestRunner(
+        tap_class=TapPostgres, config=SAMPLE_CONFIG, catalog=tap_catalog
+    )
+    test_runner.sync_all()
+    for schema_message in test_runner.schema_messages:
+        if (
+            "stream" in schema_message
+            and schema_message["stream"] == altered_table_name
+        ):
+            assert "number" in schema_message["schema"]["properties"]["column"]["type"]
+
+
+def test_filter_schemas():
+    """Only return tables from a given schema"""
+    table_name = "test_filter_schemas"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(table_name, metadata_obj, Column("id", BIGINT), schema="new_schema")
+
+    with engine.begin() as conn:
+        conn.execute(text("CREATE SCHEMA IF NOT EXISTS new_schema"))
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+    filter_schemas_config = copy.deepcopy(SAMPLE_CONFIG)
+    filter_schemas_config.update({"filter_schemas": ["new_schema"]})
+    tap = TapPostgres(config=filter_schemas_config)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"new_schema-{table_name}"
+    # Check that the only stream in the catalog is the one table put into new_schema
+    assert len(tap_catalog["streams"]) == 1
+    assert tap_catalog["streams"][0]["stream"] == altered_table_name
+
+
+class PostgresTestRunner(TapTestRunner):
+    def run_sync_dry_run(self) -> bool:
+        """
+        Dislike this function and how TestRunner does this so just hacking it here.
+        Want to be able to run exactly the catalog given
+        """
+        new_tap = self.new_tap()
+        new_tap.sync_all()
+        return True
+
+
+def test_invalid_python_dates():
+    """Some dates are invalid in python, but valid in Postgres
+
+    Check out https://www.psycopg.org/psycopg3/docs/advanced/adapt.html#example-handling-infinity-date
+    for more information.
+
+    """
+    table_name = "test_invalid_python_dates"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("date", DATE),
+        Column("datetime", DateTime),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(
+            date="4713-04-03 BC",
+            datetime="4712-10-19 10:23:54 BC",
+        )
+        conn.execute(insert)
+    tap = TapPostgres(config=SAMPLE_CONFIG)
+    # Alter config and then check the data comes through as a string
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "FULL_TABLE"
+
+    test_runner = PostgresTestRunner(
+        tap_class=TapPostgres, config=SAMPLE_CONFIG, catalog=tap_catalog
+    )
+    with pytest.raises(ValueError):
+        test_runner.sync_all()
+
+    copied_config = copy.deepcopy(SAMPLE_CONFIG)
+    # This should cause the same data to pass
+    copied_config["dates_as_string"] = True
+    tap = TapPostgres(config=copied_config)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "FULL_TABLE"
+
+    test_runner = PostgresTestRunner(
+        tap_class=TapPostgres, config=SAMPLE_CONFIG, catalog=tap_catalog
+    )
+    test_runner.sync_all()
+
+    for schema_message in test_runner.schema_messages:
+        if (
+            "stream" in schema_message
+            and schema_message["stream"] == altered_table_name
+        ):
+            assert ["string", "null"] == schema_message["schema"]["properties"]["date"][
+                "type"
+            ]
+            assert ["string", "null"] == schema_message["schema"]["properties"][
+                "datetime"
+            ]["type"]
+    assert test_runner.records[altered_table_name][0] == {
+        "date": "4713-04-03 BC",
+        "datetime": "4712-10-19 10:23:54 BC",
+    }
---
+++ tests/test_replication_key.py
@@ -0,0 +1,167 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import copy
+import json
+
+import pendulum
+import sqlalchemy
+from singer_sdk.testing.runners import TapTestRunner
+from singer_sdk.testing.templates import TapTestTemplate
+from sqlalchemy import Column, MetaData, String, Table
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+
+from tap_postgres.tap import TapPostgres
+
+TABLE_NAME = "test_replication_key"
+SAMPLE_CONFIG = {
+    "start_date": pendulum.datetime(2022, 11, 1).to_iso8601_string(),
+    "sqlalchemy_url": "postgresql://postgres:postgres@localhost:5432/postgres",
+}
+
+
+def replication_key_test(tap, table_name):
+    """Originally built to address
+    https://github.com/MeltanoLabs/tap-postgres/issues/9
+    """
+    tap.run_discovery()
+    # TODO Switch this to using Catalog from _singerlib as it makes iterating
+    # over this stuff easier
+    tap_catalog = json.loads(tap.catalog_json_text)
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            # Without this the tap will not do an INCREMENTAL sync properly
+            stream["replication_key"] = "updated_at"
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "INCREMENTAL"
+                    metadata["metadata"]["replication-key"] = "updated_at"
+
+    # Handy for debugging
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    #    json.dump(tap_catalog, f, indent=4)
+
+    tap = TapPostgres(config=SAMPLE_CONFIG, catalog=tap_catalog)
+    tap.sync_all()
+
+
+def test_null_replication_key_with_start_date():
+    """Null replication keys cause weird behavior. Check for appropriate handling.
+
+    If a start date is provided, only non-null records with an replication key value
+    greater than the start date should be synced.
+    """
+    table_name = "test_null_replication_key_with_start_date"
+    engine = sqlalchemy.create_engine(SAMPLE_CONFIG["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("data", String()),
+        Column("updated_at", TIMESTAMP),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(
+            data="Alpha", updated_at=pendulum.datetime(2022, 10, 20).to_iso8601_string()
+        )
+        conn.execute(insert)
+        insert = table.insert().values(
+            data="Bravo", updated_at=pendulum.datetime(2022, 11, 20).to_iso8601_string()
+        )
+        conn.execute(insert)
+        insert = table.insert().values(data="Zulu", updated_at=None)
+        conn.execute(insert)
+    tap = TapPostgres(config=SAMPLE_CONFIG)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            stream["replication_key"] = "updated_at"
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "INCREMENTAL"
+                    metadata["metadata"]["replication-key"] = "updated_at"
+
+    test_runner = TapTestRunner(
+        tap_class=TapPostgres,
+        config=SAMPLE_CONFIG,
+        catalog=tap_catalog,
+    )
+    test_runner.sync_all()
+    assert len(test_runner.records[altered_table_name]) == 1  # Only record Bravo.
+
+
+def test_null_replication_key_without_start_date():
+    """Null replication keys cause weird behavior. Check for appropriate handling.
+
+    If a start date is not provided, sync all records, including those with a null value
+    for their replication key.
+    """
+    table_name = "test_null_replication_key_without_start_date"
+
+    modified_config = copy.deepcopy(SAMPLE_CONFIG)
+    modified_config["start_date"] = None
+    engine = sqlalchemy.create_engine(modified_config["sqlalchemy_url"], future=True)
+
+    metadata_obj = MetaData()
+    table = Table(
+        table_name,
+        metadata_obj,
+        Column("data", String()),
+        Column("updated_at", TIMESTAMP),
+    )
+    with engine.begin() as conn:
+        if table.exists(conn):
+            table.drop(conn)
+        metadata_obj.create_all(conn)
+        insert = table.insert().values(
+            data="Alpha", updated_at=pendulum.datetime(2022, 10, 20).to_iso8601_string()
+        )
+        conn.execute(insert)
+        insert = table.insert().values(
+            data="Bravo", updated_at=pendulum.datetime(2022, 11, 20).to_iso8601_string()
+        )
+        conn.execute(insert)
+        insert = table.insert().values(data="Zulu", updated_at=None)
+        conn.execute(insert)
+    tap = TapPostgres(config=modified_config)
+    tap_catalog = json.loads(tap.catalog_json_text)
+    altered_table_name = f"public-{table_name}"
+    for stream in tap_catalog["streams"]:
+        if stream.get("stream") and altered_table_name not in stream["stream"]:
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = False
+        else:
+            stream["replication_key"] = "updated_at"
+            for metadata in stream["metadata"]:
+                metadata["metadata"]["selected"] = True
+                if metadata["breadcrumb"] == []:
+                    metadata["metadata"]["replication-method"] = "INCREMENTAL"
+                    metadata["metadata"]["replication-key"] = "updated_at"
+
+    test_runner = TapTestRunner(
+        tap_class=TapPostgres,
+        config=modified_config,
+        catalog=tap_catalog,
+    )
+    test_runner.sync_all()
+    assert len(test_runner.records[altered_table_name]) == 3  # All three records.
+
+
+class TapTestReplicationKey(TapTestTemplate):
+    name = "replication_key"
+    table_name = TABLE_NAME
+
+    def test(self):
+        replication_key_test(self.tap, self.table_name)
